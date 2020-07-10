f4f24dee79cf066f477cf397a566666ff25a0990
00145620 <00145620@uca.edu.sv>
2020-07-10 17:50:38 -0600

    Proyecto final

--- proyecto final/ProyectoFinal.cpp
+++ proyecto final/ProyectoFinal.cpp
@@ -22,7 +22,7 @@ int main(){
     cout<<"Cantidad de articulos: ";
     cin>>n;
     cout<<"\n";
-    CostoPorArticulo CPA[n];
+    CostoPorArticulo CPA[n]; //declaramos el arreglo de estructuras dentro del main para que con "n" podamos definir cuantas estructuras tendremos.
 
 
     leer(CPA, n);
@@ -34,7 +34,7 @@ int main(){
 }
 
 
-float leer(struct CostoPorArticulo p[],int n){
+float leer(struct CostoPorArticulo p[],int n){ //En esta función se pide al usuario ingresar los datos del producto.
     for(int i =0; i<n; i++){
         cout<<"Ingrese nombre del articulo: ";
         cin>> p[i].nombrearticulo;
@@ -47,17 +47,17 @@ float leer(struct CostoPorArticulo p[],int n){
 
 }
 
-float calcular(struct CostoPorArticulo p[],int n){
+float calcular(struct CostoPorArticulo p[],int n){ //calculamos el precio total de cada producto multpiplicando cantidad por precio.
     for(int i =0; i<n; i++){
         p[i].costoporarticulo = p[i].cantidad * p[i].precio;
     }
 
 }
 
-float desplegar(struct CostoPorArticulo p[],int n){
+float desplegar(struct CostoPorArticulo p[],int n){ //Con esta función desplegamos los datos introducidos por el usuario mas el costo total por articulo.
     for(int i =0; i<n; i++){
         cout<<"Nombre del articulo: "<<p[i].nombrearticulo<<"\n";
-        cout<<"Cantidad de articulos: "<<p[1].cantidad<<"\n";
+        cout<<"Cantidad de articulos: "<<p[i].cantidad<<"\n";
         cout<<"Precio de cada articulo: $"<<p[i].precio<<"\n";
         cout<<"Precio total del articulo: $"<<p[i].costoporarticulo<<"\n";
         cout<<"\n";
@@ -69,7 +69,7 @@ float total(struct CostoPorArticulo p[],int n){
     float preciototal = 0;
     for(int i =0; i<n; i++){
 
-        preciototal += p[i].costoporarticulo;
+        preciototal += p[i].costoporarticulo; //utilizamos "+=" para realizar la operación de manera mas compacta.
 
     }
     return preciototal;
